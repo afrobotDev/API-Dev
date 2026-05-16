8cf650ba67b69995eeac999355333eff6e6ce1a9
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-16 17:44:52 +0300

    Add created_at timestamp column to Post DB model

--- app/db_models.py
+++ app/db_models.py
@@ -1,5 +1,8 @@
-from sqlalchemy import Boolean, Integer, String
+from sqlalchemy import Boolean, Integer, String, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime 
+
+
 
 from app.database import Base
 
@@ -11,3 +14,4 @@ class Post(Base):
     title: Mapped[str] = mapped_column(String, nullable=False)
     content: Mapped[str] = mapped_column(String, nullable=False)
     published: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
