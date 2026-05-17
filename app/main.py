d5e1b3e8f353c1376234c957d03a08d0ae189dd1
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-17 09:32:34 +0300

    Remove auto-create tables on startup (handled by Alembic)

--- app/main.py
+++ app/main.py
@@ -1,17 +1,5 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 
-from app import db_models  # noqa: F401
-from app.database import Base, engine
 from app.routers.posts import router as posts_router
-
-
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    Base.metadata.create_all(bind=engine)
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.include_router(posts_router)
