79568ada45ab2cc1b4bdff428ec2bd51e5fe7ad7
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-18 07:52:53 +0300

    Add User model with id, email, password fields

--- app/db_models.py
+++ app/db_models.py
@@ -15,3 +15,10 @@ class Post(Base):
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), nullable=False
     )
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String, nullable=False)
