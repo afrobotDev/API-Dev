f0f1b993e71f11222a08cb225e9f1835ed089faf
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-18 07:52:56 +0300

    Add Alembic migration to create users table

---
+++ alembic/versions/6c1c303b37e2_create_users_table.py
@@ -0,0 +1,37 @@
+"""create users table
+
+Revision ID: 6c1c303b37e2
+Revises: 0d688aa2d3a0
+Create Date: 2026-05-18 00:00:00.000000
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '6c1c303b37e2'
+down_revision: Union[str, Sequence[str], None] = '0d688aa2d3a0'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("email", sa.String(), nullable=False),
+        sa.Column("password", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("email"),
+    )
+    op.create_index(op.f("ix_users_id"), "users", ["id"], unique=False)
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_index(op.f("ix_users_id"), table_name="users")
+    op.drop_table("users")
