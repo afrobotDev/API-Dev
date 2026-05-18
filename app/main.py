e5c4b137d281ab158f3efe2e0fc798b5beca34a1
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-18 13:54:09 +0300

    Register auth router in main app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
+from app.routers.auth import router as auth_router
 from app.routers.posts import router as posts_router
 from app.routers.users import router as users_router
 app = FastAPI()
+app.include_router(auth_router)
 app.include_router(posts_router)
 app.include_router(users_router)
