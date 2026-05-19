52ea192139c7929e65602de2cae0e8754179a4c4
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-19 16:34:12 +0300

    Register votes router in main app

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ from fastapi import FastAPI
 from app.routers.auth import router as auth_router
 from app.routers.posts import router as posts_router
 from app.routers.users import router as users_router
+from app.routers.votes import router as votes_router
 app = FastAPI()
 app.include_router(auth_router)
 app.include_router(posts_router)
 app.include_router(users_router)
+app.include_router(votes_router)
