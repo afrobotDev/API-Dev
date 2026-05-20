6514778482173b59d244351daef9f8a5ca385e09
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-20 16:21:57 +0300

    Fix incompatible CORS allow_origins=* with allow_credentials

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.auth import router as auth_router
 from app.routers.posts import router as posts_router
 from app.routers.users import router as users_router
 from app.routers.votes import router as votes_router
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth_router)
 app.include_router(posts_router)
 app.include_router(users_router)
